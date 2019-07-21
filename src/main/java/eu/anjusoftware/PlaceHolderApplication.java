f8e5ff51cc1f6557ab26508e8bb7f8547236ed48
Hugo Azenha Zuim <hugoazuim@gmail.com>
2019-07-21 13:13:56 +0200

    create vaadin table and call the service

---
+++ src/main/java/eu/anjusoftware/PlaceHolderApplication.java
@@ -0,0 +1,13 @@
+package eu.anjusoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceHolderApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceHolderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/eu/anjusoftware/vaadin/PlaceHolderTable.java
@@ -0,0 +1,29 @@
+package eu.anjusoftware.vaadin;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import eu.anjusoftware.dtos.PlaceHolder;
+import eu.anjusoftware.services.PlaceHolderService;
+
+@Route(value="placeholder")
+public class PlaceHolderTable extends VerticalLayout {
+	
+	private static final long serialVersionUID = 1L;
+	public final Grid<PlaceHolder> grid;
+	public PlaceHolderService placeHolderService;
+	
+	public PlaceHolderTable(PlaceHolderService placeHolderService) {
+		this.placeHolderService = placeHolderService;
+		this.grid = new Grid<>(PlaceHolder.class);
+		this.grid.setColumns("id", "userId", "title", "completed");
+		add(grid);
+		listPlaceHolders();
+	}
+	
+	private void listPlaceHolders() {
+		grid.setItems(placeHolderService.findAll());
+	}
+
+}
