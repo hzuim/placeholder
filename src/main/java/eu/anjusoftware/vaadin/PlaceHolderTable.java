664a189cb06d4fc989023955a704c64d1c264b69
Hugo Azenha Zuim <hugoazuim@gmail.com>
2019-07-22 21:05:38 +0200

    added css for table

--- src/main/java/eu/anjusoftware/vaadin/PlaceHolderTable.java
+++ src/main/java/eu/anjusoftware/vaadin/PlaceHolderTable.java
@@ -1,5 +1,6 @@
 package eu.anjusoftware.vaadin;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.gridpro.GridPro;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -8,6 +9,7 @@ import eu.anjusoftware.dtos.PlaceHolder;
 import eu.anjusoftware.services.PlaceHolderService;
 
 @Route(value="placeholder")
+@StyleSheet("placeholder.css")
 public class PlaceHolderTable extends VerticalLayout {
 	
 	private static final long serialVersionUID = 1L;
@@ -17,6 +19,9 @@ public class PlaceHolderTable extends VerticalLayout {
 	public PlaceHolderTable(PlaceHolderService placeHolderService) {
 		this.placeHolderService = placeHolderService;
 		this.grid = new GridPro<>();
+		
+		this.grid.addThemeNames("row-stripes");
+		this.grid.setId("grid");
 		this.grid.addColumn(PlaceHolder::getId).setHeader("ID");
 		this.grid.addColumn(PlaceHolder::getUserId).setHeader("User ID");
 		this.grid.addEditColumn(PlaceHolder::getTitle).text((item, newValue) -> {}).setHeader("Title (editable)");
